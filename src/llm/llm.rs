2b46f6c50cc579ae0a489659381b51dd18685379
gaowenju <gaowenju@bytedance.com>
2023-12-07 17:09:34 +0800

    feat: add llm util to caculate rough tokens count

---
+++ src/llm/llm.rs
@@ -0,0 +1,19 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+// roughly count the token count in a file.
+pub fn count_tokens_rough(path: &Path) -> io::Result<usize> {
+    let file = File::open(path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut token_count = 0;
+
+    for line in reader.lines() {
+        let line = line?;
+        let tokens: Vec<&str> = line.split(|c: char| c.is_whitespace() || c.is_ascii_punctuation()).collect();
+        token_count += tokens.len();
+    }
+
+    Ok(token_count)
+}
