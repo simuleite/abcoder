0d1d26a8c70d0b5c1675ce7773daa1242b1919c6
gaowenju <gaowenju@bytedance.com>
2023-12-08 17:30:15 +0800

    feat: add more info about function and compressed function

--- src/compress/parser.rs
+++ src/compress/parser.rs
@@ -1,7 +1,21 @@
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Function {
     pub(crate) name: String,
+    pub(crate) is_method: bool,
+    pub(crate) is_third_party: bool,
+    pub(crate) associated_struct: String,
     pub(crate) content: String,
+    pub(crate) compressed_content: CompressedFunction,
     pub(crate) calls: Vec<String>,  // names of the functions this one calls
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CompressedFunction {
+    pub(crate) function_name: String,
+    pub(crate) description: String,
+    pub(crate) input: String,
+    pub(crate) output: String,
+    pub(crate) side_effect: String,
 }
 
 pub trait LanguageParser {
