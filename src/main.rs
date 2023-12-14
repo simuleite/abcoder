3b10d93f9fe38f4ec2b377b4ef2011fde08e5612
gaowenju <gaowenju@bytedance.com>
2023-12-14 21:47:18 +0800

    feat: add ryze as http server and a simple handle

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::net::SocketAddr;
 // Import necessary types from the standard library
 use std::path::{Path, PathBuf};
 use std::ptr::addr_of_mut;
+use std::sync::Arc;
 
+use hyper::Body;
+use reqwest::Url;
+use ryze::hertz::{Hertz, RequestContext};
 use tokio::runtime::Runtime;
 
 // use crate::llm::llm;
@@ -14,11 +19,52 @@ use crate::utils::files;
 use crate::utils::git;
 use crate::utils::markdown;
 
-mod utils;
 mod compress;
+mod utils;
+
+
+// Index handler
+fn basic_info(ctx: &mut RequestContext) {
+    let parsed: std::collections::HashMap<String, String> = serde_urlencoded::from_str(ctx.req.uri().query().unwrap()).unwrap();
+
+    let repo = parsed.get("repo").unwrap();
+
+
+    println!("{}", repo);
+
+    // Url of the repository to be cloned
+    let repo_url = format!("https://github.com/{}.git", repo);
+    println!("{}", repo_url);
+
+
+    let repo_dir = format!("./tmp/{}", repo);
+    // Directory where you want to clone the repository
+    let repo_dir = Path::new(repo_dir.as_str());
+
+    // Call the git_clone function and handle any errors that occur
+    match git::git_clone(repo_url.as_str(), repo_dir) {
+        Ok(()) => println!("Git repo cloned successfully!"),
+        Err(e) => eprintln!("An error occurred while cloning the repo: {}", e),
+    }
+
+    let mut md_list = Vec::new();
+    markdown::get_all_md(repo_dir, &mut md_list).expect("TODO: panic message");
+
+
+    for md in &md_list {
+        let body = format!("{}:{:?}",
+                           md,
+                           files::count_lines(Path::new(md.as_str()), true).unwrap()).clone();
+        let body = *ctx.resp.body_mut() = Body::from(body);
+    }
+}
 
 #[tokio::main]
 async fn main() {
+    let h = Hertz::new();
+    h.get("/basic_info", Arc::new(basic_info)).await;
+    h.spin(SocketAddr::from(([0, 0, 0, 0], 8888))).await;
+
     // Url of the repository to be cloned
     let repo_url = "https://github.com/cloudwego/hertz.git";
 
@@ -31,21 +77,31 @@ async fn main() {
     //     Err(e) => eprintln!("An error occurred while cloning the repo: {}", e),
     // }
 
-
     let mut md_list = Vec::new();
     markdown::get_all_md(repo_dir, &mut md_list).expect("TODO: panic message");
 
     for md in &md_list {
-        println!("{}:{:?}", md, files::count_lines(Path::new(md.as_str()), true).unwrap())
+        println!(
+            "{}:{:?}",
+            md,
+            files::count_lines(Path::new(md.as_str()), true).unwrap()
+        )
     }
     for md in &md_list {
-        println!("{}:{:?}", md, files::count_lines(Path::new(md.as_str()), false).unwrap())
+        println!(
+            "{}:{:?}",
+            md,
+            files::count_lines(Path::new(md.as_str()), false).unwrap()
+        )
     }
 
     for md in &md_list {
-        println!("{}:{:?}", md, count_tokens_rough(Path::new(md.as_str())).unwrap())
+        println!(
+            "{}:{:?}",
+            md,
+            count_tokens_rough(Path::new(md.as_str())).unwrap()
+        )
     }
-
 
     // match git::get_repo_stats("cloudwego", "hertz").await {
     //     Ok(_) => println!("Successfully fetched repo stats"),
@@ -54,13 +110,11 @@ async fn main() {
 
     // git::search_issue("cloudwego", "hertz","server closed connection").await;
 
-
     // let mut p = golang::parser::GolangParser::new();
     // p.process();
 
-
-    let suffix = "rs";
-    let path = PathBuf::from(".");
+    let suffix = "go";
+    let path = PathBuf::from("./tmp/demo");
     if let Some(tree) = files::tree(&path, suffix) {
         println!("{}", tree);
     }
